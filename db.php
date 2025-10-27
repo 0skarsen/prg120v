fc417308c4801367f432f606421d906bb648fc7d
0skarsen <oskar.edva@gmail.com>
2025-10-27 18:05:49 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,1 @@
-<?php
-$host = "b-studentsql-1.usn.no";  // Viktig: riktig database-server
-$db = "osedv4399";                // Din database
-$user = "osedv4399";              // Ditt brukernavn
-$pass = "855osedv4399";           // Ditt passord
 
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Tilkobling feilet: " . $conn->connect_error);
-}
-?>
