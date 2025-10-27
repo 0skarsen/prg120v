7425ecf80ef4127ffba9566e091b3ebf70fa6685
0skarsen <oskar.edva@gmail.com>
2025-10-27 18:08:06 +0100

    Update vis_klasser.php

--- vis_klasser.php
+++ vis_klasser.php
@@ -1,11 +1,1 @@
-<?php
-include("db.php");
-print("<h3>Vis alle klasser</h3>");
 
-$result = $conn->query("SELECT * FROM klasse");
-echo "<table border='1'><tr><th>Kode</th><th>Navn</th><th>Studium</th></tr>";
-while ($row = $result->fetch_assoc()) {
-    echo "<tr><td>{$row['klassekode']}</td><td>{$row['klassenavn']}</td><td>{$row['studiumkode']}</td></tr>";
-}
-echo "</table>";
-?>
