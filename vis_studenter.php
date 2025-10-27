c8a0ee6c6749481c6e34b96a1f7a775663151149
0skarsen <oskar.edva@gmail.com>
2025-10-27 18:08:18 +0100

    Update vis_studenter.php

--- vis_studenter.php
+++ vis_studenter.php
@@ -1,11 +1,1 @@
-<?php
-include("db.php");
-print("<h3>Vis alle studenter</h3>");
 
-$result = $conn->query("SELECT * FROM student");
-echo "<table border='1'><tr><th>Brukernavn</th><th>Fornavn</th><th>Etternavn</th><th>Klassekode</th></tr>";
-while ($row = $result->fetch_assoc()) {
-    echo "<tr><td>{$row['brukernavn']}</td><td>{$row['fornavn']}</td><td>{$row['etternavn']}</td><td>{$row['klassekode']}</td></tr>";
-}
-echo "</table>";
-?>
