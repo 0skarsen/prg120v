1c702b549567fb66566fd6b8638b2a87b764e2c8
0skarsen <oskar.edva@gmail.com>
2025-10-27 16:43:25 +0100

    Update vis_studenter.php

--- vis_studenter.php
+++ vis_studenter.php
@@ -1,24 +1,11 @@
 <?php
-include('db.php');
+include("db.php");
+print("<h3>Vis alle studenter</h3>");
 
-$result = $mysqli->query("SELECT s.brukernavn, s.fornavn, s.etternavn, k.klassenavn 
-                          FROM student s JOIN klasse k ON s.klassekode = k.klassekode");
+$result = $conn->query("SELECT * FROM student");
+echo "<table border='1'><tr><th>Brukernavn</th><th>Fornavn</th><th>Etternavn</th><th>Klassekode</th></tr>";
+while ($row = $result->fetch_assoc()) {
+    echo "<tr><td>{$row['brukernavn']}</td><td>{$row['fornavn']}</td><td>{$row['etternavn']}</td><td>{$row['klassekode']}</td></tr>";
+}
+echo "</table>";
 ?>
-
-<h2>Alle studenter</h2>
-<table border="1">
-    <tr>
-        <th>Brukernavn</th>
-        <th>Fornavn</th>
-        <th>Etternavn</th>
-        <th>Klasse</th>
-    </tr>
-    <?php while($row = $result->fetch_assoc()): ?>
-    <tr>
-        <td><?= $row['brukernavn'] ?></td>
-        <td><?= $row['fornavn'] ?></td>
-        <td><?= $row['etternavn'] ?></td>
-        <td><?= $row['klassenavn'] ?></td>
-    </tr>
-    <?php endwhile; ?>
-</table>
