91f8e8e2ea4d20a42cb430b13aabb18899d781e2
0skarsen <oskar.edva@gmail.com>
2025-10-27 18:11:11 +0100

    Update vis_klasser.php

--- vis_klasser.php
+++ vis_klasser.php
@@ -1,1 +1,14 @@
+<?php include 'db.php'; ?>
+<!DOCTYPE html><html lang="no"><head><meta charset="UTF-8"><title>Vis alle klasser</title></head><body>
+<h2>Alle klasser</h2>
+<table border="1" cellpadding="5">
+<tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>
+<?php
+foreach ($pdo->query("SELECT * FROM klasse") as $rad) {
+  echo "<tr><td>{$rad['klassekode']}</td><td>{$rad['klassenavn']}</td><td>{$rad['studiumkode']}</td></tr>";
+}
+?>
+</table>
+<a href="index.php">Tilbake</a>
+</body></html>
 
