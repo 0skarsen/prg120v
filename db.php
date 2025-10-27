8a17bf2c4220c118dbfd65c617948498ea8996e7
0skarsen <oskar.edva@gmail.com>
2025-10-27 16:53:58 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "dokploy.usn.no";
-$db = "osedv4399";
-$user = "osedv4399";
-$pass = "855osedv4399";
+$host = "b-studentsql-1.usn.no";  // Viktig: riktig database-server
+$db = "osedv4399";                // Din database
+$user = "osedv4399";              // Ditt brukernavn
+$pass = "855osedv4399";           // Ditt passord
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
