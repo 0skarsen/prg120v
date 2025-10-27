ac51b6772ace0cdcecc2baa2b5fa381279aed474
0skarsen <oskar.edva@gmail.com>
2025-10-27 18:09:17 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,19 @@
+CREATE DATABASE IF NOT EXISTS prg120;
+USE prg120;
+
+CREATE TABLE IF NOT EXISTS klasse (
+  klassekode CHAR(5) NOT NULL,
+  klassenavn VARCHAR(50) NOT NULL,
+  studiumkode VARCHAR(50) NOT NULL,
+  PRIMARY KEY (klassekode)
+);
+
+CREATE TABLE IF NOT EXISTS student (
+  brukernavn CHAR(7) NOT NULL,
+  fornavn VARCHAR(50) NOT NULL,
+  etternavn VARCHAR(50) NOT NULL,
+  klassekode CHAR(5) NOT NULL,
+  PRIMARY KEY (brukernavn),
+  FOREIGN KEY (klassekode) REFERENCES klasse(klassekode)
+);
 
